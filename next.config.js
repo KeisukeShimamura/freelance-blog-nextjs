ee33e82d3c0dc9a3717179cea2af0fc58027babc
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2023-06-15 14:00:19 +0900

    feature：環境変数設定

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ['images.microcms-assets.io'],
   },
+  env: {
+    MICROCMS_API_KEY: process.env.MICROCMS_API_KEY,
+    MICROCMS_SERVICE_DOMAIN: process.env.MICROCMS_SERVICE_DOMAIN,
+    BASE_URL: process.env.BASE_URL,
+  },
 };
 
 module.exports = nextConfig;
